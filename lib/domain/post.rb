462989876cac0d75497e972c33afd38ccb63bb16
mingyuanyang <mingyuan7725@gmail.com>
2014-07-23 13:00:08 +0100

    Delete post.rb

--- lib/domain/post.rb
+++
@@ -1,9 +0,0 @@
-class Post < ActiveRecord::Base
-  attr_accessible :text, :title
-  has_many :commentps, dependent: :destroy
-  belongs_to :user
-  validates :title, presence: true,
-            length: { minimum: 5 }
-  validates :text, presence: true,
-            length: { minimum: 10 }
-end
