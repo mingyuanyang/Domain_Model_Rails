4a85ec6947d07dd88d9e49745a2be4f3267ef977
mingyuanyang <mingyuan7725@gmail.com>
2014-07-23 12:59:30 +0100

    Delete commentf.rb

--- lib/domain/commentf.rb
+++
@@ -1,7 +0,0 @@
-class Commentf < ActiveRecord::Base
-  belongs_to :forum
-  belongs_to :user
-  attr_accessible :body, :user_id
-  validates :body, presence: true,
-            length: { minimum: 10 }
-end
